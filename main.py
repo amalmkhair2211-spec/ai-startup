dd186f1981b96f082e5bff795da48d6c7b65e239
amalmkhair2211-spec <amalmkhair2211@gmail.com>
2026-06-06 00:45:50 +0300

    Update home route to render landing.html

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Smart Assistant Hub is running 🚀"
+    return render_template("landing.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
